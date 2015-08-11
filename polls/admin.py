2e3d5c8c6b88e7c9472be60b70b393e1ced611d7
Tenzin Kunkyab <ten.kunkyab@gmail.com>
2015-08-11 11:00:52 +0530

    new commits, part 4 of djangodocs

--- polls/admin.py
+++ polls/admin.py
@@ -16,5 +16,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInLine]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
